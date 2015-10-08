32766598904e4b547d9ec53dc422cfe6963ba62d
Tyler Harbert <harbertt11@gmail.com>
2015-10-08 11:31:12 -0400

    Adding multiple truth tables to make several different vhdl files

--- main.cpp
+++ main.cpp
@@ -8,9 +8,21 @@ using namespace std;
 
 int main()
 {
-    Parser* p = new Parser("/Users/harbertt11/Documents/CodeBlocksProjects/TT_to_VHDL/","test.txt", "out.vhd");
+    Parser* p = new Parser("/Users/harbertt11/Documents/CodeBlocksProjects/TT_to_VHDL/Files/","unsigned_3bit.txt", "unsigned_3bit.vhd");
     p->parse();
-    p->generateVHDL("squarer");
+    p->generateVHDL("unsigned_squarer_3bit");
+
+    Parser* p1 = new Parser("/Users/harbertt11/Documents/CodeBlocksProjects/TT_to_VHDL/Files/","signed_3bit.txt", "signed_3bit.vhd");
+    p1->parse();
+    p1->generateVHDL("signed_squarer_3bit");
+
+    Parser* p2 = new Parser("/Users/harbertt11/Documents/CodeBlocksProjects/TT_to_VHDL/Files/","unsigned_4bit.txt", "unsigned_4bit.vhd");
+    p2->parse();
+    p2->generateVHDL("unsigned_squarer_4bit");
+
+    Parser* p3 = new Parser("/Users/harbertt11/Documents/CodeBlocksProjects/TT_to_VHDL/Files/","signed_4bit.txt", "signed_4bit.vhd");
+    p3->parse();
+    p3->generateVHDL("signed_squarer_4bit");
 
     return 0;
 }
